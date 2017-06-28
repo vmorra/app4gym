75a2a4a109b903d55b2350ab9ed12c69c9fca653
Morra <v.morra@accenture.com>
2017-06-28 17:28:19 +0200

    First FE Commit

---
+++ app4gym/src/components/auth.js
@@ -0,0 +1,27 @@
+class AuthService{
+
+  static requireAuth() {
+    if (!sessionStorage.getItem('jwttoken')){
+      //console.log("Not Authenticated User");
+      //console.log(JSON.stringify(sessionStorage));
+      return true;
+    }
+    else{
+      //console.log("Authenticated User");
+      //console.log(JSON.stringify(sessionStorage));
+      return false;
+    }
+  }
+
+  static populateUserSession(data){
+    sessionStorage.setItem('jwttoken', data.token);
+    sessionStorage.setItem('uprofile', data.user);
+  }
+
+  static clearUserSession(){
+    sessionStorage.removeItem('jwttoken');
+    sessionStorage.removeItem('uprofile');
+  }
+}
+
+export { AuthService as default}
