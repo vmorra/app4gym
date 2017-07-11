0231deea26af03308919f24b8b93b50a5609f7fa
DIR\ivan.mascolo <ivan.mascolo@accenture.com>
2017-07-12 00:01:06 +0200

    add google-invisible-recaptcha on Register.js

--- app4gym/src/views/Pages/Register/Register.js
+++ app4gym/src/views/Pages/Register/Register.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
+import Recaptcha from 'react-google-invisible-recaptcha';
+import {default as UUID} from "node-uuid";
+import { Redirect } from 'react-router';
 
 class Register extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      accountname: '',
+      email:'',
+      password: '',
+      repeatpassword: ''
+    };
+    this.onSubmit = this.onSubmit.bind( this );
+    this.onResolved = this.onResolved.bind( this );
+    this.handleChangeAccountName = this.handleChangeAccountName.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeRepeatPassword = this.handleChangeRepeatPassword.bind(this);
+  }
+
+  handleChangeAccountName(event) {
+    console.log(event.target.value);
+    this.setState({accountname: event.target.value});
+  }
+
+  handleChangeEmail(event) {
+    console.log(event.target.value);
+    this.setState({email: event.target.value});
+  }
+
+  handleChangePassword(event) {
+    console.log(event.target.value);
+    this.setState({password: event.target.value});
+  }
+  handleChangeRepeatPassword(event) {
+    console.log(event.target.value);
+    if ( this.state.password == event.target.value ) {
+      this.setState({repeatpassword: event.target.value});
+    }
+  }
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -13,21 +52,25 @@ class Register extends Component {
                   <p className="text-muted">Create your account</p>
                   <div className="input-group mb-3">
                     <span className="input-group-addon"><i className="icon-user"></i></span>
-                    <input type="text" className="form-control" placeholder="Username"/>
+                    <input type="text" className="form-control" placeholder="Username" onChange={this.handleChangeAccountName}/>
                   </div>
                   <div className="input-group mb-3">
                     <span className="input-group-addon">@</span>
-                    <input type="text" className="form-control" placeholder="Email"/>
+                    <input type="text" className="form-control" placeholder="Email" onChange={this.handleChangeEmail}/>
                   </div>
                   <div className="input-group mb-3">
                     <span className="input-group-addon"><i className="icon-lock"></i></span>
-                    <input type="password" className="form-control" placeholder="Password"/>
+                    <input type="password" className="form-control" placeholder="Password" onChange={this.handleChangePassword}/>
                   </div>
                   <div className="input-group mb-4">
                     <span className="input-group-addon"><i className="icon-lock"></i></span>
-                    <input type="password" className="form-control" placeholder="Repeat password"/>
+                    <input type="password" className="form-control" placeholder="Repeat password" onChange={this.handleChangeRepeatPassword}/>
                   </div>
-                  <button type="button" className="btn btn-block btn-success">Create Account</button>
+                  <Recaptcha
+                    ref={ ref => this.recaptcha = ref }
+                    sitekey="6LcDvCgUAAAAAOOFtq4_mp_Fq5yNBtuVQlyHp-kZ"
+                    onResolved={ this.onResolved } />
+                  <button type="button" className="btn btn-block btn-success" onClick={ this.onSubmit }>Create Account</button>
                 </div>
                 <div className="card-footer p-4">
                   <div className="row">
@@ -46,6 +89,17 @@ class Register extends Component {
       </div>
     );
   }
+  onSubmit() {
+   if ( '' == this.state.accountname || '' == this.state.email || '' == this.state.password || '' == this.state.repeatpassword ) {
+     alert( 'Validation failed! Inputs cannot be empty.' );
+     this.recaptcha.reset();
+   } else {
+     this.recaptcha.execute();
+   }
+ }
+ onResolved() {
+   alert( 'Recaptcha resolved with response: ' + this.recaptcha.getResponse() );
+ }
 }
 
 export default Register;
