5505f92e9fa1f5e56ccfa033b5d5d617620c0496
Roberto <Roberto@roberto-pc.homenet.telecomitalia.it>
2017-07-27 23:37:03 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'develop' of https://github.com/vmorra/app4gym.git into
    develop
    
    Conflicts:
    	app4gym/js/controllers/login.js
    	app4gym/js/routes.js
    	app4gym/views/pages/login.html

--- app4gym/js/controllers/register.js
+++ app4gym/js/controllers/register.js
@@ -6,8 +6,44 @@ controller('RegisterCtrl', RegisterCtrl)
 RegisterCtrl.$inject = ['$scope', '$http'];
 function RegisterCtrl($scope, $http) {
 
-  $scope.submit = function(event){
-      event.preventDefault();
-      alert("sono uno che....");
-  }
+	 $scope.user = {
+			 
+			  "i_account_name": "",
+			  "i_password": "",
+			  "a_email": [
+			    ""
+			  ],
+			  "type": "Tecnico",
+			  "status":"Pending",
+			  "roles": [
+			    {
+			      "code": 1,
+			      "label": "User",
+			      "description": "string"
+			    }
+			    ]
+			  
+	  };
+	  
+	  $scope.config = {
+			  headers: {
+				  "Content-Type":"application/json"
+			  }
+	  }
+	  
+	  $scope.submit = function(){
+		  $http({
+			  method: 'POST',
+			  url: 'http://localhost:3001/api/auth/signup',
+			  data: $scope.user,
+			  config : $scope.headers
+			}).then(function successCallback(response) {
+			    console.log("response data:" +JSON.stringify(response.data))
+			    $state.transitionTo('appSimple.login');
+			  }, function errorCallback(response) {
+				  
+				  console.log("response data:"+ JSON.stringify(response.data));
+				  console.log("response status:"+ JSON.stringify(response.status))
+			  });
+	  }
 }
